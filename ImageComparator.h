08419b4eec4216dbfa2f99a882fb5e7157d9b689
Torben <87072972+kipteamm@users.noreply.github.com>
2025-03-30 16:49:21 +0200

    Update ImageComparator.h

--- ImageComparator.h
+++ ImageComparator.h
@@ -13,11 +13,14 @@ class ImageComparator {
 public:
     explicit ImageComparator(const std::string& directory);
 
-    static bool compareImages(const std::string& file1, const std::string& file2);
+    bool compareImages(const std::string& file1, const std::string& file2);
     void compare();
 
 private:
     std::vector<std::pair<std::string, std::string>> filePairs;
+
+    unsigned int goodMatches = 0;
+    double similarityRate = 100.0;
 };
 
 
